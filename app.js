3ce73d80e5ce15dc17d0e3aac240de9453749e4d
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2023-04-20 08:15:58 -0400

    ride routes applied

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const initMongo = require('./MongoDb')
 const userRouter = require("./Routes/HitChikerUserRoutes")
+const rideRoutes = require("./Routes/RideRoutes")
 require('dotenv').config()
 
 
@@ -17,6 +18,7 @@ app.use(express.json());
 
 // register course routes
 app.use("/users", userRouter);
+app.use("/rides", rideRoutes);
 
 
 // welcome route
