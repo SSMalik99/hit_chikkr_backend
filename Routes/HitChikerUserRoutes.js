fe2058062400832d73bac11721526603181df8ef
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2023-04-20 10:06:27 -0400

    route changed

--- Routes/HitChikerUserRoutes.js
+++ Routes/HitChikerUserRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router()
 
 router
     .post("/", createUser) // create user
-    .get("/:id", getUser) //get single user
+    .get("/:email", getUser) //get single user
     
 
 module.exports = router
